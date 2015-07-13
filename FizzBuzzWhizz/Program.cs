050daac3f5b849319cfa0ef47285a037fb1b7a34
Liu Xia <lxconan@gmail.com>
2015-07-13 10:45:37 +0800

    finish.

---
+++ FizzBuzzWhizz/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzzWhizz
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            var keys = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            for (var i = 0; i < 100; i++)
+            {
+                Console.Out.WriteLine(GetWord(i + 1, keys));
+            }
+        }
+
+        public static string GetWord(int num, int[] keys)
+        {
+            if (num.ToString().Contains(keys[0].ToString())) return "Fizz";
+
+            var word = GetWord(num, keys[0], "Fizz")
+                + GetWord(num, keys[1], "Buzz")
+                + GetWord(num, keys[2], "Whizz");
+
+            return word == "" ? num.ToString() : word;
+        }
+
+        private static string GetWord(int num, int key, string word)
+        {
+            return num % key == 0 ? word : "";
+        }
+    }
+}
---
+++ FizzBuzzWhizz/ProgramFacts.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace FizzBuzzWhizz
+{
+    public class ProgramFacts
+    {
+        [Theory]
+        [InlineData(1, "1", new[] {3, 5, 7})]
+        [InlineData(3, "Fizz", new[] {3, 5, 7})]
+        [InlineData(6, "Fizz", new[] {3, 5, 7})]
+        [InlineData(5, "Buzz", new[] {3, 5, 7})]
+        [InlineData(10, "Buzz", new[] {3, 5, 7})]
+        [InlineData(7, "Whizz", new[] {3, 5, 7})]
+        [InlineData(14, "Whizz", new[] {3, 5, 7})]
+        [InlineData(15, "FizzBuzz", new[] {3, 5, 7})]
+        [InlineData(21, "FizzWhizz", new[] {3, 5, 7})]
+        [InlineData(60, "FizzBuzzWhizz", new[] {3, 4, 5})]
+        [InlineData(13, "Fizz", new[] {3, 4, 5})]
+        [InlineData(35, "Fizz", new[] {3, 4, 5})]
+        public void should_get_word(int num, string word, int[] keys)
+        {
+            Assert.Equal(word, Program.GetWord(num, keys));
+        }
+    }
+}
